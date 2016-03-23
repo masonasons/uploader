4385683e458ffa1e4c336812b6c823d1a3647171
Mason Armstrong <masonarm@gmail.com>
2016-03-23 17:39:01 -0500

    Adds tweet feature.

--- uploader.py
+++ uploader.py
@@ -1,3 +1,5 @@
+import tweepy
+import webbrowser
 import config_utils
 import os.path as path
 import sys
@@ -6,6 +8,8 @@ import urllib
 import requests
 import wx
 
+tw1="MneWylqkykEp95neIxCvN1i2J"
+tw2="jPdug6Dl9IWxJvtsaQRqT120jYKf8bXl3drKFshw8JzGQCm6XX"
 services = []
 services.append("SNDUp")
 services.append("TWUp")
@@ -38,6 +42,14 @@ class AudioUploader(wx.Frame):
 		self.upload.Bind(wx.EVT_BUTTON, self.StartUpload)
 		self.main_box.Add(self.upload, 0, wx.ALL, 10)
 		self.upload.Hide()
+		self.twitter_label = wx.StaticText(self.panel, -1,"Tweet &Text")
+		self.twitter_text = wx.TextCtrl(self.panel, -1, "")
+		self.main_box.Add(self.twitter_text, 0, wx.ALL, 10)
+		self.twitter_text.Hide()
+		self.tweet = wx.Button(self.panel, -1, "&Tweet")
+		self.tweet.Bind(wx.EVT_BUTTON, self.Tweet)
+		self.tweet.Hide()
+		self.main_box.Add(self.tweet, 0, wx.ALL, 10)
 		self.new = wx.Button(self.panel, -1, "&Attach another file")
 		self.new.Bind(wx.EVT_BUTTON, self.Reset)
 		self.main_box.Add(self.new, 0, wx.ALL, 10)
@@ -53,6 +65,8 @@ class AudioUploader(wx.Frame):
 		self.select_file.Hide()
 		self.upload.Hide()
 		self.key.Hide()
+		self.twitter_text.Show()
+		self.tweet.Show()
 
 		if self.services.GetValue()=="SNDUp":
 			r=requests.post("http://www.sndup.net/post.php", files={"file":open(self.filename,'rb')}, data={'api_key':self.key.GetValue()})
@@ -71,7 +85,24 @@ class AudioUploader(wx.Frame):
 		self.name=path.basename(self.filename)
 		self.upload.Show()
 
+	def Tweet(self, event):
+		auth = tweepy.OAuthHandler(tw1, tw2)
+		if appconfig["general"]["TWKey"]=="" or appconfig["general"]["TWSecret"]=="":
+			webbrowser.open(auth.get_authorization_url())
+			verifier = ask(message='Enter pin:')
+			auth.get_access_token(verifier)
+			appconfig["general"]["TWKey"]=auth.access_token
+			appconfig["general"]["TWSecret"]=auth.access_token_secret
+			appconfig.write()
+		else:
+			auth.set_access_token(appconfig["general"]["TWKey"],appconfig["general"]["TWSecret"])
+		api = tweepy.API(auth)
+		api.update_status(self.twitter_text.GetValue()+" "+self.link.GetValue())
+
 	def Reset(self, event):
+		self.twitter_text.SetValue("")
+		self.twitter_text.Hide()
+		self.tweet.Hide()
 		self.services.Show()
 		self.upload.Hide()
 		self.new.Hide()
@@ -98,6 +129,12 @@ def handle_URL(url):
 		final_url = urllib.parse.unquote(url)
 	return final_url
 
+def ask(parent=None, message='', default_value=''):
+	dlg = wx.TextEntryDialog(parent, message, defaultValue=default_value)
+	dlg.ShowModal()
+	result = dlg.GetValue()
+	dlg.Destroy()
+	return result
 app = wx.App(redirect=False)
 window=AudioUploader("Audio uploader")
 window.Show()
